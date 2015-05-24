c229910ddd874669924bee992a232252f0d94bcd
mcguffin <joern@flyingletters.com>
2015-05-24 13:29:44 +0200

    Fix client side image resize

--- client-side-image-resize.php
+++ client-side-image-resize.php
@@ -7,6 +7,54 @@ Author URI: http://github.org/mcguffin
 Version: 0.0.1
 */
 
+/**
+ * Return all available image sizes.
+ * (Copied from http://codex.wordpress.org/Function_Reference/get_intermediate_image_sizes)
+ *
+ */
+if ( ! function_exists('get_image_sizes') ) :
+function get_image_sizes( $size = '' ) {
+
+	global $_wp_additional_image_sizes;
+
+	$sizes = array();
+	$get_intermediate_image_sizes = get_intermediate_image_sizes();
+
+	// Create the full array with sizes and crop info
+	foreach( $get_intermediate_image_sizes as $_size ) {
+
+		if ( in_array( $_size, array( 'thumbnail', 'medium', 'large' ) ) ) {
+
+			$sizes[ $_size ]['width'] = get_option( $_size . '_size_w' );
+			$sizes[ $_size ]['height'] = get_option( $_size . '_size_h' );
+			$sizes[ $_size ]['crop'] = (bool) get_option( $_size . '_crop' );
+
+		} elseif ( isset( $_wp_additional_image_sizes[ $_size ] ) ) {
+
+			$sizes[ $_size ] = array( 
+			'width' => $_wp_additional_image_sizes[ $_size ]['width'],
+			'height' => $_wp_additional_image_sizes[ $_size ]['height'],
+			'crop' =>  $_wp_additional_image_sizes[ $_size ]['crop']
+			);
+
+		}
+
+	}
+
+	// Get only 1 size if found
+	if ( $size ) {
+		if( isset( $sizes[ $size ] ) ) {
+			return $sizes[ $size ];
+		} else {
+			return false;
+		}
+	}
+
+	return $sizes;
+}
+endif;
+
+
 /*
 This one works on the Media upload page.
 */
@@ -37,7 +85,7 @@ This one is for the js media library
 function client_side_resize_load() {
 	wp_enqueue_script( 'client-resize' , plugins_url( 'client-side-image-resize.js' , __FILE__ ) , array('media-editor' ) , '0.0.1' );
 	wp_localize_script( 'client-resize' , 'client_resize' , array( 
-		'plupload' => enable_client_resize_plupload_params( array() ) 
+		'plupload' => client_side_resize_plupload_params( array() ) 
 	) );
 }
 
