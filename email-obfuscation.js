818a444e8c3a2417fc503a8b49e3614bf2680ca5
mcguffin <joern@flyingletters.com>
2015-05-24 13:29:56 +0200

    add email obfuscation

---
+++ email-obfuscation.js
@@ -0,0 +1,17 @@
+(function($){
+	function load_fragments(e) {
+		var req = {
+			'action':'get_email_fragments'
+		};
+		$.post(email_obfuscator.ajax_url,req,function(response){
+			$('[data-load-fragment]').each(function(){
+				var hash = $(this).data('load-fragment');
+				$(this).replaceWith(response[hash]);
+			})
+		})
+	}
+	$(document).one('click','[data-load-fragment]',load_fragments)
+	$(document).ready(function(){
+		setTimeout(load_fragments,500);
+	});
+})(jQuery);
