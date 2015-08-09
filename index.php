2c751d77e472bed058f046f46e928a9fed5a1109
mcguffin <joern@flyingletters.com>
2015-08-09 12:03:07 +0200

    load text domain later

--- index.php
+++ index.php
@@ -7,4 +7,7 @@ Author URI: http://github.org/mcguffin
 Version: 0.0.1
 */
 
-load_muplugin_textdomain( 'mu-plugins', 'languages' );
+function muplugins_load_textdomain() { 
+	load_muplugin_textdomain( 'mu-plugins', 'languages' );
+}
+add_action( 'plugins_loaded' , 'muplugins_load_textdomain' );
