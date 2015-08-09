37e1127158ac60503022f3cd2afb381e2a9877f4
mcguffin <joern@flyingletters.com>
2015-08-09 12:22:05 +0200

    add upload dir ssl fix

---
+++ wp-upload-dir-ssl-fix.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+Plugin Name: Fix WP Core issue #25449
+Plugin Description: wp_upload_dir() doesn't support https, https://core.trac.wordpress.org/ticket/25449
+Author: Jörn Lund
+Author URI: http://github.org/mcguffin
+Version: 0.0.1
+*/
+
+function _wp_fix_issue_25449($upload_dir) {
+	if ( is_ssl() ) {
+		$upload_dir['url']     = set_url_scheme( $upload_dir['url'] , 'https' );
+		$upload_dir['baseurl'] = set_url_scheme( $upload_dir['baseurl'] , 'https' );
+	} else {
+		$upload_dir['url']     = set_url_scheme( $upload_dir['url'] , 'http' );
+		$upload_dir['baseurl'] = set_url_scheme( $upload_dir['baseurl'] , 'http' );
+	}
+	return $upload_dir;
+}
+
+add_filter( 'upload_dir' , '_wp_fix_issue_25449' );
