de1a128659ebaafff9c01e8634504e6992074ddb
mcguffin <joern@flyingletters.com>
2016-01-03 15:47:42 +0100

    add disable comments (3rd party)

--- index.php
+++ index.php
@@ -11,3 +11,11 @@ function muplugins_load_textdomain() {
 	load_muplugin_textdomain( 'mu-plugins', 'languages' );
 }
 add_action( 'plugins_loaded' , 'muplugins_load_textdomain' );
+
+
+
+/**
+ *	Disable comments
+ *	git@github.com:solarissmoke/disable-comments-mu.git
+ */
+include dirname(__FILE__) .  '/disable-comments-mu/disable-comments-mu.php';
