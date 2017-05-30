914dc20982701898d93aa79b5cf2e86ec78fbe4c
mcguffin <joern@flyingletters.com>
2017-05-30 14:36:29 +0200

    update email-obfuscation

--- email-obfuscation.php
+++ email-obfuscation.php
@@ -5,15 +5,15 @@ Plugin Name: Email Obfuscation
 Description: Hides email addresses in the HTML code.
 Author: Jörn Lund
 Author URI: http://github.org/mcguffin
-Version: 0.0.2
+Version: 0.0.3
 */
 
 class EmailObfuscator {
 	private $map_fragments = array();
 	private static $_instance = null;
-	
-	private $pattern_email = "/[a-zA-Z0-9\.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*/s";
-	private $pattern_link = '/<a([^>]*)href=([\'"]{1})mailto:.+\/a>/imsU';
+
+	private $pattern_email	= "/(.{7})[a-zA-Z0-9\.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*/s";
+	private $pattern_link	= '/<a([^>]*)href=([\'"]{1})mailto:.+\/a>/imsU';
 	/**
 	 * Getting a singleton.
 	 *
@@ -41,6 +41,10 @@ class EmailObfuscator {
 		add_action( 'wp_ajax_nopriv_get_email_fragments' , array( &$this , 'ajax_get_fragments' ) );
 	}
 	
+
+	/**
+	 *	@action wp_head
+	 */
 	function enqueue_script() {	
 		wp_register_script( 'email-obfuscation' , plugins_url( 'email-obfuscation.js' , __FILE__ ) , array( 'jquery' )  );
 		wp_localize_script( 'email-obfuscation' , 'email_obfuscator' , array(
@@ -48,28 +52,45 @@ class EmailObfuscator {
 		) );
 		wp_enqueue_script( 'email-obfuscation' );
 	}
+
+	/**
+	 *	@action wp_ajax_get_email_fragments
+	 *	@action wp_ajax_nopriv_get_email_fragments
+	 */
 	function ajax_get_fragments( ) {
 		$file = $this->_get_cache_file( $_SERVER['HTTP_REFERER'] );
 		header('Content-Type: application/json');
-		if ( file_exists( $file ) )
+		if ( file_exists( $file ) ) {
 			echo file_get_contents( $file );
+		}
 		exit();
 	}
+
+	/**
+	 *	@action shutdown
+	 */
 	function store_fragments() {
-		if ( ! wp_mkdir_p( $this->cachepath ) )
+		if ( ! wp_mkdir_p( $this->cachepath ) ) {
 			wp_die('Can\'t write to cache dir');
-		
+		}
 		file_put_contents( $this->_get_cache_file() , json_encode( $this->map_fragments ) );
 	}
 
+	/**
+	 *	@action template_redirect
+	 */
 	function start_output( ) {
 		ob_start( array( &$this , 'filter_output' ) );
 	}
 	
-	
+	/**
+	 *	@param string $output
+	 *	@return string Page content with obfuscated email addresses
+	 */
 	function filter_output( $output ) {
-		if ( empty( $output ) )
+		if ( empty( $output ) ) {
 			return '';
+		}
 		// source: http://www.w3.org/TR/html5/forms.html#valid-e-mail-address
 		$output = preg_replace_callback( '@<head(.*)</head>@imsU' , array( &$this , '_filter_head' ) , $output );
 		$output = preg_replace_callback( '@<body(.*)</body>@imsU' , array( &$this , '_filter_body' ) , $output );
@@ -78,12 +99,24 @@ class EmailObfuscator {
 		return $output;
 	}
 	
+	/**
+	 *	preg_replace_callback
+	 *
+	 *	@param array $matches
+	 *	@return string 
+	 */
 	private function _filter_head( $matches ){
 		$output = $matches[0];
 		$output = preg_replace_callback( $this->pattern_email , array( &$this , '_replace_email_address_head' ) , $output );
 		return $output;
 	}
 	
+	/**
+	 *	used by preg_replace_callback
+	 *
+	 *	@param array $matches
+	 *	@return string 
+	 */
 	private function _filter_body( $matches ) {
 		$output = $matches[0];
 		$output = preg_replace_callback( $this->pattern_link , array( &$this , '_replace_email_link' ) , $output );
@@ -91,39 +124,83 @@ class EmailObfuscator {
 		return $output;
 	}
 	
+	/**
+	 *	used by preg_replace_callback
+	 *
+	 *	@param array $matches
+	 *	@return string 
+	 */
 	private function _replace_email_link( $matches ) {
 		$hash = md5($matches[0]);
 		$this->map_fragments[$hash] = $matches[0];
 		return sprintf( '<a href="#%s" data-load-fragment="%s">%s</a>' , $hash , $hash , __('(Click to show Email Link)' , 'mu-plugins' ) );
 	}
+
+	/**
+	 *	used by preg_replace_callback
+	 *
+	 *	@param array $matches
+	 *	@return string 
+	 */
 	private function _replace_email_address( $matches ) {
-		$hash = md5($matches[0]);
-		$this->map_fragments[$hash] = $matches[0];
-		return sprintf( '<a href="#%s" data-load-fragment="%s">%s</a>' , $hash , $hash , __('(Click to show Email Address)' , 'mu-plugins' ) );
+		if ( strpos( $matches[1], 'value' ) === 0 ) {
+			return $matches[0];
+		}
+		$before = $matches[1];
+		$email = substr($matches[0],7);
+		$hash = md5( $email );
+
+		$this->map_fragments[$hash] = $email;
+		return $before . sprintf( '<a href="#%s" data-load-fragment="%s">%s</a>' , $hash , $hash , __('(Click to show Email Address)' , 'mu-plugins' ) );
 	}
 	
+	/**
+	 *	used by preg_replace_callback
+	 *
+	 *	@param array $matches
+	 *	@return string 
+	 */
 	private function _replace_email_address_head( $matches ) {
 		return __('(Undisclosed Email Address)','mu-plugins');
 	}
 	
 
-	private function _get_cache_file( $url = '' ) {
+	/**
+	 *	@param string $url
+	 *	@return string  filepath
+	 */
+	private function _get_cache_file( $url = null ) {
+
 		return $this->cachepath . DIRECTORY_SEPARATOR . $this->_get_hash( $url ) . '.json';
 	}
 	
+	/**
+	 *	@param string $url if empty current request is used
+	 *	@return string  url hash
+	 */
 	private function _get_hash( $url = '' ) {
 		// make sure to get rid of the port part in $_SERVER['HTTP_HOST']
-		$hash_url = empty($url) ? ( parse_url('http://'.$_SERVER['HTTP_HOST'],PHP_URL_HOST) . $_SERVER['REQUEST_URI'] ) : ( parse_url($url, PHP_URL_HOST) . parse_url($url, PHP_URL_PATH) );
+		if ( empty( $url ) ) {
+			$url = $_SERVER['REQUEST_URI'];
+		}
+		$hash_url = ( parse_url($url, PHP_URL_PATH) . '?' . parse_url( $url, PHP_URL_QUERY ) );
 		return md5( $hash_url . NONCE_SALT );
 	}
 	
 	
+	/**
+	 *	@return null
+	 */
 	function install() {
-		if ( ! wp_mkdir_p( $this->cachepath ) )
+		if ( ! wp_mkdir_p( $this->cachepath ) ) {
 			wp_die('Cannot write to cache dir');
+		}
+
 		$htaccess = $this->cachepath . DIRECTORY_SEPARATOR.'.htaccess';
-		if ( ! file_exists( $htaccess ) && ! file_put_contents( $htaccess , 'deny from all'."\n" ) )
+
+		if ( ! file_exists( $htaccess ) && ! file_put_contents( $htaccess , 'deny from all'."\n" ) ) {
 			wp_die('Cannot create .htaccess in cache dir');
+		}
 	}
 }
 
@@ -131,8 +208,6 @@ EmailObfuscator::instance();
 
 // Source: http://www.ex-parrot.com/pdw/Mail-RFC822-Address.html
 // '/(?:(?:\r\n)?[ \t])*(?:(?:(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*))*@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*|(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)*\<(?:(?:\r\n)?[ \t])*(?:@(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*(?:,@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*)*:(?:(?:\r\n)?[ \t])*)?(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*))*@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*\>(?:(?:\r\n)?[ \t])*)|(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)*:(?:(?:\r\n)?[ \t])*(?:(?:(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*))*@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*|(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)*\<(?:(?:\r\n)?[ \t])*(?:@(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*(?:,@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*)*:(?:(?:\r\n)?[ \t])*)?(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*))*@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*\>(?:(?:\r\n)?[ \t])*)(?:,\s*(?:(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*))*@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*|(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)*\<(?:(?:\r\n)?[ \t])*(?:@(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*(?:,@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*)*:(?:(?:\r\n)?[ \t])*)?(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|"(?:[^\"\r\\]|\\.|(?:(?:\r\n)?[ \t]))*"(?:(?:\r\n)?[ \t])*))*@(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*)(?:\.(?:(?:\r\n)?[ \t])*(?:[^()<>@,;:\\".\[\] \000-\031]+(?:(?:(?:\r\n)?[ \t])+|\Z|(?=[\["()<>@,;:\\".\[\]]))|\[([^\[\]\r\\]|\\.)*\](?:(?:\r\n)?[ \t])*))*\>(?:(?:\r\n)?[ \t])*))*)?;\s*)/'
-
-
 
 
 // Source: 
