bce01c6134164b1da2907159c433891b6c52e6b5
mcguffin <joern@flyingletters.com>
2017-06-02 11:25:52 +0200

    rm disable comments

--- index.php
+++ index.php
@@ -14,13 +14,3 @@ function muplugins_load_textdomain() {
 add_action( 'plugins_loaded' , 'muplugins_load_textdomain' );
 
 
-
-/**
- *	Disable comments
- *	git@github.com:solarissmoke/disable-comments-mu.git
- */
-include dirname(__FILE__) .  '/disable-comments-mu/disable-comments-mu.php';
-add_action('widgets_init',function() {
-	include_once dirname(__FILE__) .  '/widgets/PostType_Taxonomy_Widget.php';
-	register_widget( 'PostType_Taxonomy_Widget' );
-});
