db787cdc73f62cb60e715905af6e53905356b7f1
mcguffin <joern@flyingletters.com>
2018-03-09 13:15:19 +0100

    wp_profiler > memory_get_usage()

--- debug.php
+++ debug.php
@@ -5,7 +5,7 @@ Plugin Name: Debugging tools
 Description: Profiler, disable server cache, show DB queries
 Author: Jörn Lund
 Author URI: http://github.org/mcguffin
-Version: 0.0.2
+Version: 0.0.3
 */
 
 /**
@@ -25,15 +25,16 @@ class WP_Profiler {
 	private static $_instance=null;
 	private $started = 0;
 	private $entries = 0;
-	
+
 	public static function instance(){
 		if ( is_null( self::$_instance ) )
 			self::$_instance = new self();
 		return self::$_instance;
 	}
-	
+
 	private function __construct( ) {
 		$this->started = microtime( true );
+		$this->mem_started = memory_get_usage();
 		$this->entries = array();
 		if ( defined('WP_DEBUG') && WP_DEBUG && function_exists('add_action') ) {
 			add_action( 'shutdown' , array( __CLASS__ , 'dump' ) );
@@ -41,26 +42,35 @@ class WP_Profiler {
 	}
 	public static function log( $what ){
 		self::instance()->entries[] = array(
-			'what' => $what,
-			'time' => microtime( true ),
+			'what'	=> $what,
+			'time'	=> microtime( true ),
+			'mem'	=> memory_get_usage(),
 		);
 	}
 	public static function dump( ) {
 		$last = 0;
+		$mlast = self::instance()->mem_started;
 		if ( count( self::instance()->entries ) ) {
 			self::log('Shutdown');
 			?><pre><?php
-			echo "TIME                  | TIME SINCE LAST        |  \n";
+			echo "TIME                 | TIME SINCE LAST      | MEMORY       |  MEMORY DIFF | WHAT  \n";
 			foreach ( self::instance()->entries as $i => $entry ) {
 				$t = $entry['time'] - self::instance()->started;
 				$diff = $t - $last;
-				echo str_pad( number_format( $t , 16 , '.' , '') , 20 ,STR_PAD_LEFT);
-				echo "  |  ";
-				echo str_pad( number_format( $diff , 16 , '.' , '') , 20 ,STR_PAD_LEFT);
-				echo "  |  ";
+				$m = $entry['mem'];
+				$mdiff = $m - $mlast;
+				echo str_pad( number_format( $t , 16 , '.' , '') , 20, ' ', STR_PAD_RIGHT);
+				echo " | ";
+				echo str_pad( number_format( $diff , 16 , '.' , '') , 20, ' ', STR_PAD_RIGHT);
+				echo " | ";
+				echo str_pad( $m , 12 ,' ', STR_PAD_LEFT);
+				echo " | ";
+				echo str_pad( $mdiff , 12 ,' ', STR_PAD_LEFT);
+				echo " | ";
 				echo $entry['what'];
 				echo "\n";
 				$last = $t;
+				$mlast = $m;
 			}
 			?></pre><?php
 		}
@@ -91,7 +101,7 @@ if ( defined('WP_DEBUG') && WP_DEBUG && ! is_admin() ) {
 if ( defined('SAVEQUERIES') && SAVEQUERIES ) :
 	function show_queries() {
 		if (  defined('WP_DEBUG') && WP_DEBUG &&
-			! defined('DOING_AJAX') && 
+			! defined('DOING_AJAX') &&
 			(strpos($_SERVER['SERVER_NAME'],'.local') !== false || current_user_can( 'administrator' ) )) {
 			global $wpdb;
 			echo "<pre style=\"padding-left:180px;background:#fff;color:#333;white-space:pre-wrap\">";
@@ -102,7 +112,7 @@ if ( defined('SAVEQUERIES') && SAVEQUERIES ) :
 	}
 	add_action( 'shutdown' , 'show_queries' );
 	add_action( 'show_queries' , 'show_queries' );
-	
+
 endif;
 
 
