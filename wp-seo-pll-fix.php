9e41eae96a6a7bc4eae250cff13433b74f0f0618
mcguffin <joern@flyingletters.com>
2018-11-27 10:40:15 +0100

    refactor wp-seo-pll-fix

--- wp-seo-pll-fix.php
+++ wp-seo-pll-fix.php
@@ -4,19 +4,21 @@ Plugin Name: WP-SEO / Polylang fix
 Description: Fix missing SEO-Columns after quick edit when Polylang is enabled
 Author: Jörn Lund
 Author URI: http://github.org/mcguffin
-Version: 0.0.1
+Version: 0.0.2
 */
 
 // Init WP-SEO Admin class when PLL is saving
-function wpseo_ppl_admin_init() {
-	if ( function_exists( 'wpseo_admin_init' ) ) {
-		wpseo_admin_init();
-	}
-}
+
 
 if ( defined( 'DOING_AJAX' ) && DOING_AJAX && filter_input( INPUT_POST, 'action' ) === 'pll_update_post_rows' ) {
-	add_action( 'plugins_loaded', 'wpseo_ppl_admin_init', 15 );
+
+	// init wp-seo admin class
+	add_action( 'plugins_loaded', function() {
+		if ( function_exists( 'wpseo_admin_init' ) ) {
+			wpseo_admin_init();
+		}
+	}, 15 );
+
 	// Always show WP-SEO admin cols.
 	add_filter( 'wpseo_always_register_metaboxes_on_admin', '__return_true' );
 }
-
